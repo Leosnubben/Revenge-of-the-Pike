dcba32c144bf638cb10ca82c47b34b5e8e5fb640
20Simon17 <98454367+20Simon17@users.noreply.github.com>
2022-10-19 16:34:08 +0200

    arbete

---
+++ Assets/Scripts/FishSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawner : MonoBehaviour
+{
+    public GameObject[] fishes;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
