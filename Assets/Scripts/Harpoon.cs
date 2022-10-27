2d740ae64351b47913a47df4ae6aafcad2110c54
20Simon17 <98454367+20Simon17@users.noreply.github.com>
2022-10-27 09:16:14 +0200

    harpoonmove

--- Assets/Scripts/Harpoon.cs
+++ Assets/Scripts/Harpoon.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Harpoon : MonoBehaviour
 {
     public GameObject harpoon;
+
+    float shootCooldown = 2f;
     void Start()
     {
         
@@ -13,9 +15,12 @@ public class Harpoon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        shootCooldown -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && shootCooldown <= 0f)
         {
             Instantiate(harpoon, new Vector3(transform.position.x, 0.3f, 0), Quaternion.identity);
+            shootCooldown = 2f;
         }
     }
 }
