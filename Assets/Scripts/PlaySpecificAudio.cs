f02bd5e450fbf8654508a9250043ab18249f04a4
LilTravz <97680020+LilTravz@users.noreply.github.com>
2022-10-27 09:03:32 +0200

    Audio trigger

---
+++ Assets/Scripts/PlaySpecificAudio.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySpecificAudio : MonoBehaviour
+{
+    [SerializeField] private AudioSource myAudioSource;
+    [SerializeField] private AudioClip myAudioClip1;
+    [SerializeField] private float volume = 1.0f;
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!myAudioSource.isPlaying)
+            {
+                myAudioSource.PlayOneShot(myAudioClip1, volume);
+            }
+            else
+            {
+                myAudioSource.Pause();
+                myAudioSource.Play();
+            }
+        }
+    }
+}
